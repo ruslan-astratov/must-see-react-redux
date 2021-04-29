7a70caf09a0d4918d5357cb00b40fd12a0644a5b
Ruslan Astratov <63612588+ruslan-astratov@users.noreply.github.com>
2021-04-29 08:39:15 +0200

    Add files via upload

---
+++ precache-manifest.2bdfa41711b737dc74ae75336c1a6b38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "badbeb25946a50b2de2e3cb309128724",
+    "url": "/must-see/index.html"
+  },
+  {
+    "revision": "b5fa9c7da69e95d0930e",
+    "url": "/must-see/static/css/main.cb385a34.chunk.css"
+  },
+  {
+    "revision": "d97e6b15434da4b808de",
+    "url": "/must-see/static/js/2.8c8cad38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/must-see/static/js/2.8c8cad38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5fa9c7da69e95d0930e",
+    "url": "/must-see/static/js/main.2ceeb1cf.chunk.js"
+  },
+  {
+    "revision": "852137381cf513fc9883",
+    "url": "/must-see/static/js/runtime-main.9c1efa88.js"
+  }
+]);
